5d925844e68c4c1e4311553bd129e50eb7a4f11e
shriya747492 <shriya747492@gmail.com>
2017-02-15 21:36:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
  var element = document.getElementById("main-text");
  
  element.innerHTML= "New Value";
+ 
+ var element1 = document.getElementById("move_image");
+ 
+ element1.onclick= function(){
+     element1.style.marginleft = '100px';
+ };
